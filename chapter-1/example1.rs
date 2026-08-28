a0808498f8dcfdc28ad38862dcc8f1ed4b05d639
Volkan K. <7557299+kumbasar@users.noreply.github.com>
2026-08-28 18:20:59 +0300

    example1.rs

---
+++ chapter-1/example1.rs
@@ -0,0 +1,19 @@
+/*
+fn main() {
+    let my_string: String = "Volkan";
+    my_string.push("!");
+    println!("{}" my_string)
+}
+*/
+
+fn main() {
+    let mut my_string: String = "Volkan".to_string();
+    my_string.push('!');
+    println!("{}", my_string)
+}
+
+/* Output:
+
+Volkan!
+
+*/
