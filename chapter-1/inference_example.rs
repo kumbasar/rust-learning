7675f11db48988b4a3fee6680e7985a2796b8e24
Volkan K. <7557299+kumbasar@users.noreply.github.com>
2026-09-01 11:02:27 +0300

    inference_example.rs

---
+++ chapter-1/inference_example.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let _small_number_1: u8 = 10;
+    let _small_number_2 = 10u8;
+    let _small_number_3 = 10_u8;
+    let _big_number_1 = 100_000_000_i32;
+
+    let number_1 = 0___u8;
+    let number_2 = 1___6____22___3__i32;
+
+    println!("{}, {}", number_1, number_2);
+}
+
+/*
+0, 16223
+ */
