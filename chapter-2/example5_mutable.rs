d1e2dbb9e1cd147b9c82ae3b804a1727b51960e2
Volkan K. <7557299+kumbasar@users.noreply.github.com>
2026-09-02 15:26:21 +0300

    Update example5_mutable.rs

--- chapter-2/example5_mutable.rs
+++ chapter-2/example5_mutable.rs
@@ -7,9 +7,19 @@ fn main() {
     let second_number = 800;
     let triple_ref = &&&second_number;
     println!("Are they equal? {}", second_number == ***triple_ref);
+
+
+    let mut number = 10;
+    let number_change = &mut number;
+    *number_change += 10; // mutable reference
+    let number_ref = &number; // immutable reference
+    println!("{}", number_ref);
+
+
 }
 
 /*
 num ref: 18
 Are they equal? true
+20
  */
