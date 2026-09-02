8278110d859362882ddca14dfa2c7e065c79581c
Volkan K. <7557299+kumbasar@users.noreply.github.com>
2026-09-02 10:22:41 +0300

    Create example3_const_static.rs

---
+++ chapter-2/example3_const_static.rs
@@ -0,0 +1,17 @@
+const NUMBER_OF_MONTHS: u32 = 12; //Don't change
+static _SEASONS: [&str; 4] = ["Spring", "Summer", "Fall", "Winter"]; //Can be changed
+
+fn print_months() {
+    println!("Number of months in a year: {NUMBER_OF_MONTHS}");
+}
+
+fn main() {
+    print_months();
+}
+
+/*
+Number of months in a year: 12
+
+Process finished with exit code 0
+
+ */
